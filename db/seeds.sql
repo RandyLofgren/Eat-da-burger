28e351c83c4e0ebec19b34dd8c58ab6c72b95f06
RandyLofgren <43276499+RandyLofgren@users.noreply.github.com>
2020-12-14 16:52:53 -0500

    grabbed skeleton from cats and did a few steps

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Avocado and Jalapeno Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Impossible Burger');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo Chicken');
+INSERT INTO burgers (burger_name) VALUES ('Prime Rib Steak Burger');
