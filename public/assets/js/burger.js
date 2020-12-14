1f861abbdeab88ad218567dc50498de4cb35d974
RandyLofgren <43276499+RandyLofgren@users.noreply.github.com>
2020-12-14 18:40:14 -0500

    deleted some delete info

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -39,18 +39,4 @@ $(function() {
     );
   });
 
-  $(".delete-cat").on("click", function(event) {
-    var id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted cat", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
 });
